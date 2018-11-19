3c5a31c14ec1df8de1001581f2d80df741f4c0e2
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-11-19 19:24:40 +0200

    Bug Fixes

--- src/controller.js
+++ src/controller.js
@@ -5,34 +5,25 @@ const stringify = require('csv-stringify');
 
 router.get('/', (req, res) => {
   const handles = req.query.handles.split(';').map(handle => handle.toLowerCase().trim());
-  console.log(handles);
-  const data = database
-    .filter(entry => handles.indexOf(entry.handle.toLowerCase()) !== -1)
-    .map(entry => ({
-      handle: entry.handle.toLowerCase(),
-      'Contest 1': entry['Contest 1'] ? entry['Contest 1'].points : 0,
-      'Contest 2': entry['Contest 2'] ? entry['Contest 2'].points : 0,
-      'Contest 3': entry['Contest 3'] ? entry['Contest 3'].points : 0,
-      'Contest 4': entry['Contest 4'] ? entry['Contest 4'].points : 0,
-      'Contest 1 Details': entry['Contest 1'],
-      'Contest 2 Details': entry['Contest 2'],
-      'Contest 3 Details': entry['Contest 3'],
-      'Contest 4 Details': entry['Contest 4']
-    }));
 
-  data.sort((a, b) => handles.indexOf(a.handle) - handles.indexOf(b.handle));
-
-  for (let i = 0; i < handles.length; ++i) {
-    if (data.length <= i || data[i].handle !== handles[i]) {
-      data.splice(i, 0, {
-        handle: handles[i],
-        'Contest 1': 0,
-        'Contest 2': 0,
-        'Contest 3': 0,
-        'Contest 4': 0
-      });
+  const data = handles.map(handle => {
+    const entry = database.filter(entry => entry.handle.toLowerCase() === handle)[0];
+    if (entry === undefined) {
+      return { handle, 'Contest 1': 0, 'Contest 2': 0, 'Contest 3': 0, 'Contest 4': 0 };
+    } else {
+      return {
+        handle,
+        'Contest 1': entry['Contest 1'] ? entry['Contest 1'].points : 0,
+        'Contest 2': entry['Contest 2'] ? entry['Contest 2'].points : 0,
+        'Contest 3': entry['Contest 3'] ? entry['Contest 3'].points : 0,
+        'Contest 4': entry['Contest 4'] ? entry['Contest 4'].points : 0,
+        'Contest 1 Details': entry['Contest 1'],
+        'Contest 2 Details': entry['Contest 2'],
+        'Contest 3 Details': entry['Contest 3'],
+        'Contest 4 Details': entry['Contest 4']
+      };
     }
-  }
+  });
 
   const columns = [
     'handle',
